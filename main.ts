1547df50572c05164e87ca8b3911526fa05475bd
Guillaume-Legault <93203429+Guillaume-Legault@users.noreply.github.com>
2021-11-30 15:17:15 -0500

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,69 @@
+function Cycle_pieton () {
+    fin_cycle_pieton = 0
+    debut_cycle_pieton = 1
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.pause(5000)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    for (let index = 0; index < 5; index++) {
+        pins.digitalWritePin(DigitalPin.P8, 1)
+        basic.pause(500)
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        basic.pause(500)
+    }
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    debut_cycle_pieton = 0
+    fin_cycle_pieton = 1
+    basic.pause(100)
+}
+input.onButtonPressed(Button.A, function () {
+    Pieton = 1
+})
+function Cycle_lumière () {
+    fin_cycle_lumiere = 0
+    debut_cycle_lumiere = 1
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    basic.pause(15000)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    basic.pause(5000)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    basic.pause(10000)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    debut_cycle_lumiere = 0
+    fin_cycle_lumiere = 1
+    basic.pause(100)
+}
+let temps = 0
+let fin_cycle_pieton = 0
+let fin_cycle_lumiere = 0
+let debut_cycle_pieton = 0
+let debut_cycle_lumiere = 0
+let Pieton = 0
+radio.setGroup(16)
+Pieton = 0
+debut_cycle_lumiere = 0
+debut_cycle_pieton = 0
+fin_cycle_lumiere = 0
+fin_cycle_pieton = 0
+basic.forever(function () {
+    if (debut_cycle_pieton == 1) {
+        for (let index = 0; index <= 10; index++) {
+            temps = index * -1 + 10
+            basic.showNumber(temps)
+        }
+    }
+})
+basic.forever(function () {
+    if (Pieton == 1 && debut_cycle_lumiere == 0) {
+        Cycle_pieton()
+        Pieton = 0
+    } else if (Pieton == 0 && debut_cycle_pieton == 0) {
+        Cycle_lumière()
+    } else {
+    	
+    }
+})
